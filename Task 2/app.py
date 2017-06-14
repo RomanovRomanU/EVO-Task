528f6713fa26724cada0d1ae17465fd640cbb965
Roman Romanov <Roman Romanov>
2017-06-14 17:58:22 +0300

    Main task of Task 2 was done

---
+++ Task 2/app.py
@@ -0,0 +1,77 @@
+from flask import Flask
+from flask import render_template, request, url_for, abort
+from flask_sqlalchemy import SQLAlchemy
+from hashlib import sha256
+
+ALLOWED_EXTENSIONS = ('txt', 'pdf', 'doc', 'docx', 'jpg')
+app = Flask(__name__,static_url_path = "/static")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////documents.db'
+db = SQLAlchemy(app)
+
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # Sha will represent like hex (length = 64)
+    hex_sha = db.Column(db.String(64), unique=True)
+    # How many times this document was uploaded
+    count = db.Column(db.Integer)
+
+    def __init__(self, hex_sha):
+        self.hex_sha = hex_sha
+        self.count = 1
+
+    def __repr__(self):
+        return 'SHA of file is: %s' % self.hex_sha
+
+
+def allowed_filename(filename):
+    if '.' not in filename:
+        return False
+    extension = filename.split('.')[1].lower()
+    if extension not in ALLOWED_EXTENSIONS:
+        return False
+    else:
+        return True
+
+
+# Route for rendering all appointments in database
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/download', methods=['POST'])
+def download_file():
+    # If there is no file dowloaded
+    if 'file' not in request.files:
+        return
+    file = request.files['file']
+    # If the file isn`t empty and having proper filename
+    if file and allowed_filename(file.filename):
+        # Reading the content of file
+        file_repr = str(file.stream.read())
+        # Convert it to utf-8
+        # Ignore means,that we pass chars,that we can`t convert
+        file_repr = file_repr.encode('utf-8')
+        file_hex_sha = sha256()
+        file_hex_sha.update(file_repr) 
+        file_hex_sha = file_hex_sha.hexdigest()
+        # Searching for the same document in database
+        db_file = Document.query.filter_by(hex_sha=file_hex_sha).first()
+        # If there is no such document in database
+        if db_file is None:
+            new_db_file = Document(file_hex_sha)
+            db.session.add(new_db_file)
+            db.session.commit()
+            return str(file_hex_sha)+" "+str(new_db_file.count)
+        # If we found same document
+        else:
+            db_file.count += 1
+            db.session.commit()
+            return str(file_hex_sha)+" "+str(db_file.count)
+    else:
+        # Bad request
+        abort(400)
+
+if __name__ == '__main__':
+    app.run()
---
+++ Task 2/static/js/uploadFile.js
@@ -0,0 +1,36 @@
+var submitButton = document.getElementById('formSubmit');
+// When upload button is clicked
+submitButton.addEventListener('click', function (e) {
+	if (e.preventDefault) {
+		e.preventDefault();
+	}
+	// Take our uploaded file
+	var file = document.getElementById('file-1').files[0];
+	// Wraping our file to formdata
+	var formData = new FormData();
+	formData.append('file',file);
+	// Set up the request.
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST','/download',true);
+	xhr.onload = function(){
+		// If everything is fine
+		if (xhr.status === 200) {
+			// Response will be string of such type:
+			// "SHA fileCounter"
+			var response = xhr.responseText.split(' ');
+			var sha = response[0];
+			// How many times this file was uploaded
+			var fileCounter = response[1];
+			// Recording this values to page
+			document.getElementById('sha').innerHTML = `<i>SHA256 is:</i> ${sha}`;
+			document.getElementById('fileCounter').innerHTML = `<i>Was downloaded:</i>${fileCounter} times.`;
+		}
+		// If our file is incorrect
+		if (xhr.status === 400) {
+			alert(
+				'The file format or content is incorrect.Try again'
+				);
+		}
+	}
+	xhr.send(formData);
+});
