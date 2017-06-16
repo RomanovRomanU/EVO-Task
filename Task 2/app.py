3266a42dfdbffaea0d0b03098f37abbc42900084
Roman Romanov <Roman Romanov>
2017-06-16 13:14:25 +0300

    CRC-16 hash on client side implemented

--- Task 2/app.py
+++ Task 2/app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask import render_template, request, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from hashlib import sha256
+from threading import Lock
 
 ALLOWED_EXTENSIONS = ('txt', 'pdf', 'doc', 'docx', 'jpg')
 app = Flask(__name__,static_url_path = "/static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////documents.db'
 db = SQLAlchemy(app)
-
+lock = Lock()
 
 class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,25 +54,41 @@ def download_file():
         # Convert it to utf-8
         # Ignore means,that we pass chars,that we can`t convert
         file_repr = file_repr.encode('utf-8')
-        file_hex_sha = sha256()
-        file_hex_sha.update(file_repr) 
+        file_hex_sha = sha256(file_repr)
+        # file_hex_sha.update(file_repr)
         file_hex_sha = file_hex_sha.hexdigest()
+        lock.acquire()
         # Searching for the same document in database
         db_file = Document.query.filter_by(hex_sha=file_hex_sha).first()
+        # DEBUG
+        debug = (str(Document.query.all()))
         # If there is no such document in database
         if db_file is None:
             new_db_file = Document(file_hex_sha)
             db.session.add(new_db_file)
             db.session.commit()
-            return str(file_hex_sha)+" "+str(new_db_file.count)
+            response = '%s %s' % (
+                str(new_db_file.hex_sha), str(new_db_file.count)
+                )
+            response += " %s" % str(new_db_file.id)
+            response += " " + debug
+            lock.release()
+            return response
         # If we found same document
         else:
             db_file.count += 1
+            new_count = db_file.count
             db.session.commit()
-            return str(file_hex_sha)+" "+str(db_file.count)
+            response = '%s %s' % (str(db_file.hex_sha), str(new_count))
+            response += " %s" % str(db_file.id)
+            response += " " + debug
+            lock.release()
+            return response
     else:
         # Bad request
         abort(400)
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    db.create_all()
+    app.run(host='0.0.0.0', port=8080, threaded=False)
--- Task 2/static/js/uploadFile.js
+++ Task 2/static/js/uploadFile.js
@@ -1,36 +1,44 @@
-var submitButton = document.getElementById('formSubmit');
+const submitButton = document.getElementById('formSubmit');
 // When upload button is clicked
-submitButton.addEventListener('click', function (e) {
+submitButton.addEventListener('click', e => {
 	if (e.preventDefault) {
 		e.preventDefault();
 	}
-	// Take our uploaded file
-	var file = document.getElementById('file-1').files[0];
-	// Wraping our file to formdata
-	var formData = new FormData();
-	formData.append('file',file);
-	// Set up the request.
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST','/download',true);
-	xhr.onload = function(){
-		// If everything is fine
+    // Take our uploaded file
+	const file = document.getElementById('file-1').files[0];
+    // Wraping our file to formdata
+	const formData = new FormData();
+	formData.append('file', file);
+    // Set up the request.
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', '/download', true);
+	xhr.onload = function () {
+        // If everything is fine
 		if (xhr.status === 200) {
-			// Response will be string of such type:
-			// "SHA fileCounter"
-			var response = xhr.responseText.split(' ');
-			var sha = response[0];
-			// How many times this file was uploaded
-			var fileCounter = response[1];
-			// Recording this values to page
-			document.getElementById('sha').innerHTML = `<i>SHA256 is:</i> ${sha}`;
-			document.getElementById('fileCounter').innerHTML = `<i>Was downloaded:</i>${fileCounter} times.`;
+            // Response will be string of such type:
+            // "SHA fileCounter"
+			const response = xhr.responseText.split(' ');
+			const sha = response[0];
+            // How many times this file was uploaded
+			const fileCounter = response[1];
+            // Now lets count MD4 hashon client side
+			cilent_crc16 = hash_crc16(file);
+            // Recording this values to page
+			document.getElementById('sha256').innerHTML = `<p><b>SHA256 is:</b></p> ${sha}`;
+			document.getElementById('crc16').innerHTML = `<p><b>MD4 id:</b></p> ${cilent_crc16}`;
+			document.getElementById('fileCounter').innerHTML = `<p><b>Was downloaded:</b></p>${fileCounter} times.`;
 		}
-		// If our file is incorrect
+        // If our file is incorrect
 		if (xhr.status === 400) {
 			alert(
-				'The file format or content is incorrect.Try again'
-				);
+                'The file format or content is incorrect.Try again'
+            );
 		}
-	}
+	};
 	xhr.send(formData);
 });
+
+function hash_crc16(file) {
+	file = String(file);
+	return crc16(file);
+}
